6946df041ba503e1cfcadd4e1bcb4d26b0412fbe
Sarah Propst <116668788+spropst4@users.noreply.github.com>
2023-09-01 09:44:45 -0400

    Add files via upload

--- FilamentWidth/Gradient_infill_Square_Lattice.py
+++ FilamentWidth/Gradient_infill_Square_Lattice.py
@@ -5,8 +5,92 @@ Date: 8/31/23
 # This function creates a gradient infill of a square lattice
 
 import numpy as np
+def setpress(pressure):
+    # IMPORTS
+    from codecs import encode
+    from textwrap import wrap
 
-def Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gradient_region):
+    pressure = str(int(pressure * 10))
+    length = len(pressure)
+    while length < 4:
+        pressure = "0" + pressure
+        length = len(pressure)
+
+    commandc = bytes(('08PS  ' + pressure), "utf-8")
+
+    # FIND CHECKSUM
+    startc = b'\x05\x02'
+    endc = b'\x03'
+
+    hexcommand = encode(commandc, "hex")  # encode should turn this into a hex rather than ascii
+
+    hexcommand = hexcommand.decode("utf-8")  # decode should turn this into a string object rather than a bytes object
+
+    ####format for arduino#####
+    format_command = str(hexcommand)
+    format_command = '\\x'.join(format_command[i:i + 2] for i in range(0, len(format_command), 2))
+    format_command = '\\x'+format_command
+    ##########################
+
+    hexcommand = wrap(hexcommand,
+                      2)  # wrap should split the string into a horizontal array of strings of 2 characters each
+
+    # GETTING THE 8 BIT 2'S COMPLEMENT
+    decimalsum = 0
+    for i in hexcommand:  # get the decimal sum of the hex command
+        decimalsum = decimalsum + int(i, 16)
+    checksum = decimalsum % 256  # get the remainder of the decimal sum
+    checksum = bin(checksum)  # turn into binary
+    checksum = checksum[2:]  # checksum is a string
+    while len(checksum) < 8:  # checksum must represents 8 bits of information
+        checksum = "0" + checksum
+    invert = ""
+    for i in checksum:  # binary sum must be inverted
+        if i == '0':
+            invert = invert + "1"
+        else:
+            invert = invert + "0"
+    invert = int(invert, 2)  # binary sum turned into decimal form
+    invert = invert + 1
+    # CHECKSUM HAS BEEN RETRIEVED IN DECIMAL FORM
+    checksum = invert
+    checksum = hex(checksum)  # checksum is in the format "0x##"
+    # CHECKSUM IS NOW IN ASCII FORM, don't be mislead by the hex function
+    checksum = checksum[2:]
+    checksumarray = []
+    for i in checksum:  # must get alphabetical characters in uppercase for ascii to hex conversion
+        if i.isalpha():
+            i = i.upper()
+            checksumarray.append(i)
+        else:
+            checksumarray.append(i)
+    checksum = ""
+    for i in checksumarray:
+        checksum = checksum + i
+    # checksum is a string.
+    checksum = bytes(checksum, 'ascii')
+
+    ####format for arduino#####
+    hexchecksum = encode(checksum, 'hex')
+    hexchecksum = hexchecksum.decode("utf-8")  # decode should turn this into a string object rather than a bytes object
+    format_checksum = str(hexchecksum)  # format for arduino
+    format_checksum = '\\x'.join(format_checksum[i:i + 2] for i in range(0, len(format_checksum), 2))
+    format_checksum = '\\x' + format_checksum
+
+    # SENDING OUT THE COMMAND
+    ##format for arduino####
+    finalcommand = ("\\x05\\x02") + format_command + format_checksum + str("\\x03")
+    finalcommand = finalcommand.strip('\r').strip('\n')
+    finalcommand = "b'" + finalcommand + "'"
+    return finalcommand
+def togglepress():
+    # IMPORTS
+    import serial
+    from codecs import encode
+    from textwrap import wrap
+    toggle = str("b'\\x05\\x02\\x30\\x34\\x44\\x49\\x20\\x20\\x43\\x46\\x03'")
+    return toggle
+def Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gradient_region, valveON, valveOFF):
     input_variables = input_line[0]
     input_dist = input_line[1]
 
@@ -40,18 +124,27 @@ def Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gr
             sign = input_dist[0] / abs(input_dist[0])
             distance_count += segment_len
 
+            valve_toggleOFF = '\n'
+            valve_toggleON = '\n'
 
             if segment_len < distance_count <= pressure_gradient_region + segment_len:
+                valve_toggleOFF = valveOFF
+                valve_toggleON = valveON
                 pressure -= pressure_incr
 
             if distance_count > (line_length - pressure_gradient_region) and pressure < pressure_range[1]:
                 pressure += pressure_incr
 
+            pressure = round(pressure, 2)
+            f.write(valve_toggleOFF)
+            f.write(str('\n\r' + com + '.write(' + str(setpress(pressure)) + ')'))
+            f.write(valve_toggleON)
+
             print('Pressure = ', pressure)
             if (i+1) == num_segments or (i+1) == 1:
-                print('G1 ' + input_variables[0] + str(sign * first_n_last_segment_len))
+                f.write('\nG1 ' + input_variables[0] + str(sign * first_n_last_segment_len))
             else:
-                print('G1 ' + input_variables[0] + str(sign * segment_len))
+                f.write('\nG1 ' + input_variables[0] + str(sign * segment_len))
 
 
 
@@ -67,17 +160,25 @@ def Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gr
                 a_segment = first_n_last_segment_len / np.sqrt(1 + line_slope ** 2)
                 b_segment = line_slope * a_segment
 
-            if (i+1) < pressure_gradient_region:
+            ivalve_toggleOFF = '\n'
+            valve_toggleON = '\n'
+
+            if segment_len < distance_count <= pressure_gradient_region + segment_len:
+                valve_toggleOFF = valveOFF
+                valve_toggleON = valveON
                 pressure -= pressure_incr
 
-
-            elif (i+1) < line_length - pressure_gradient_region:
+            if distance_count > (line_length - pressure_gradient_region) and pressure < pressure_range[1]:
                 pressure += pressure_incr
 
+            pressure = round(pressure, 2)
+            f.write(valve_toggleOFF)
+            f.write(str('\n\r' + com + '.write(' + str(setpress(pressure)) + ')'))
+            f.write(valve_toggleON)
+
             print('Pressure = ', pressure)
-            print('change pressure')
-            print('G1 ' + input_variables[0] + str(a_sign*a_segment) + ' ' + input_variables[1] + str(b_sign*b_segment))
-def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range):
+            f.write('\nG1 ' + input_variables[0] + str(a_sign*a_segment) + ' ' + input_variables[1] + str(b_sign*b_segment))
+def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range, valveON, valveOFF):
     length = ((num_filaments-1)*fil_spacing)
     distance_list = []
     var_list = []
@@ -103,7 +204,7 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
                 num_pressure_regions -= 2
                 pressure_gradient_region -= fil_spacing
 
-            segments = ['length', fil_spacing]
+            segments = ['length', fil_spacing/3]
             #print(pressure_gradient_region)
             if (layer + 1)%2 != 0: # odd layer
                 if layer > 0:
@@ -125,7 +226,7 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
 
                 #print(fil+1, seg_number)
                 input_line = [['X'], [x_dist]]
-                Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gradient_region)
+                Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gradient_region, valveON, valveOFF)
 
                 # distance_list.append([x_dist])
                 # var_list.append(['X'])
@@ -133,7 +234,7 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
                 if (fil + 1) != num_filaments:
                     distance_list.append([y_dist])
                     var_list.append(['Y'])
-                    print('G1 Y' + str(y_dist))
+                    f.write('\nG1 Y' + str(y_dist))
 
             else:
                 x_sign_even_row = x_sign_odd_row * -1  # reverses the x-direction
@@ -146,29 +247,59 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
                 y_dist = y_sign_even_row * length
 
                 input_line = [['Y'], [y_dist]]
-                Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gradient_region)
+                Gradient_line_segmentation(input_line, segments, pressure_range, pressure_gradient_region, valveON, valveOFF)
 
                 # distance_list.append([y_dist])
                 # var_list.append(['Y'])
                 if (fil + 1) != num_filaments:
                     distance_list.append([x_dist])
                     var_list.append(['X'])
-                    print('G1 X' + str(x_dist))
+                    f.write('\nG1 X' + str(x_dist))
 
         distance_list.append([layer_height])
         var_list.append(['Z'])
 
-        print('G1 Z' +str(layer_height))
+        f.write('\nG1 Z' +str(layer_height))
     # for i in range(len(distance_list)):
     #     print('G1 ' + str(var_list[i][0]) + str(distance_list[i][0]))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    ### File names
+    export_file = '230901_GradientInfillV1_10Layers_2mmspacing_21numfilaments_pressure22_30.txt'
+    save_path = ''  # 'C:\\Users\\MuellerLab_HPC\\PycharmProjects\\Gcode_generator\\SPropst_Decoupling'
+
+    ### Geometric Settings
     fil_spacing = 1.5
     num_filaments = 15
     num_layers = 10
     fil_width = 1
     layer_height = 1
     pressure_range = [25, 45]
-    gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range)
+
+    ### Pressure box and valve settings
+    com = "serialPort1"
+    valve = 6
+
+    setpress_start = str('\n\r' + com + '.write(' + str(setpress(pressure_range[1])) + ')')  # material 1
+    toggleON = str('\n\r' + com + '.write(' + str(togglepress()) + ')')  # turn on material 2
+    toggleOFF = toggleON
+
+    valveON = '\n{aux_command}WAGO_ValveCommands(' + str(valve) + ', True)'
+    valveOFF = '\n{aux_command}WAGO_ValveCommands(' + str(valve) + ', False)'
+
+    import os.path
+
+    completeName = os.path.join(save_path, export_file)
+    f = open(completeName, "w")
+    f.write(setpress_start)
+    f.write(toggleON)
+    f.write(valveON)
+
+    gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range, valveON,valveOFF)
+
+    f.write(valveOFF)
+    f.write(toggleOFF)
+
+    f.close()
--- FilamentWidth/Gradient_infill_Square_Lattice_V2.py
+++ FilamentWidth/Gradient_infill_Square_Lattice_V2.py
@@ -6,7 +6,92 @@ Date: 8/31/23
 
 import numpy as np
 
-def Gradient_line_segmentation(input_line, segments, pressure_range):
+def setpress(pressure):
+    # IMPORTS
+    from codecs import encode
+    from textwrap import wrap
+
+    pressure = str(int(pressure * 10))
+    length = len(pressure)
+    while length < 4:
+        pressure = "0" + pressure
+        length = len(pressure)
+
+    commandc = bytes(('08PS  ' + pressure), "utf-8")
+
+    # FIND CHECKSUM
+    startc = b'\x05\x02'
+    endc = b'\x03'
+
+    hexcommand = encode(commandc, "hex")  # encode should turn this into a hex rather than ascii
+
+    hexcommand = hexcommand.decode("utf-8")  # decode should turn this into a string object rather than a bytes object
+
+    ####format for arduino#####
+    format_command = str(hexcommand)
+    format_command = '\\x'.join(format_command[i:i + 2] for i in range(0, len(format_command), 2))
+    format_command = '\\x'+format_command
+    ##########################
+
+    hexcommand = wrap(hexcommand,
+                      2)  # wrap should split the string into a horizontal array of strings of 2 characters each
+
+    # GETTING THE 8 BIT 2'S COMPLEMENT
+    decimalsum = 0
+    for i in hexcommand:  # get the decimal sum of the hex command
+        decimalsum = decimalsum + int(i, 16)
+    checksum = decimalsum % 256  # get the remainder of the decimal sum
+    checksum = bin(checksum)  # turn into binary
+    checksum = checksum[2:]  # checksum is a string
+    while len(checksum) < 8:  # checksum must represents 8 bits of information
+        checksum = "0" + checksum
+    invert = ""
+    for i in checksum:  # binary sum must be inverted
+        if i == '0':
+            invert = invert + "1"
+        else:
+            invert = invert + "0"
+    invert = int(invert, 2)  # binary sum turned into decimal form
+    invert = invert + 1
+    # CHECKSUM HAS BEEN RETRIEVED IN DECIMAL FORM
+    checksum = invert
+    checksum = hex(checksum)  # checksum is in the format "0x##"
+    # CHECKSUM IS NOW IN ASCII FORM, don't be mislead by the hex function
+    checksum = checksum[2:]
+    checksumarray = []
+    for i in checksum:  # must get alphabetical characters in uppercase for ascii to hex conversion
+        if i.isalpha():
+            i = i.upper()
+            checksumarray.append(i)
+        else:
+            checksumarray.append(i)
+    checksum = ""
+    for i in checksumarray:
+        checksum = checksum + i
+    # checksum is a string.
+    checksum = bytes(checksum, 'ascii')
+
+    ####format for arduino#####
+    hexchecksum = encode(checksum, 'hex')
+    hexchecksum = hexchecksum.decode("utf-8")  # decode should turn this into a string object rather than a bytes object
+    format_checksum = str(hexchecksum)  # format for arduino
+    format_checksum = '\\x'.join(format_checksum[i:i + 2] for i in range(0, len(format_checksum), 2))
+    format_checksum = '\\x' + format_checksum
+
+    # SENDING OUT THE COMMAND
+    ##format for arduino####
+    finalcommand = ("\\x05\\x02") + format_command + format_checksum + str("\\x03")
+    finalcommand = finalcommand.strip('\r').strip('\n')
+    finalcommand = "b'" + finalcommand + "'"
+    return finalcommand
+def togglepress():
+    # IMPORTS
+    import serial
+    from codecs import encode
+    from textwrap import wrap
+    toggle = str("b'\\x05\\x02\\x30\\x34\\x44\\x49\\x20\\x20\\x43\\x46\\x03'")
+    return toggle
+def Gradient_line_segmentation(input_line, segments, pressure_range, valveON, valveOFF,):
     input_variables = input_line[0]
     input_dist = input_line[1]
 
@@ -48,17 +133,27 @@ def Gradient_line_segmentation(input_line, segments, pressure_range):
             sign = input_dist[0] / abs(input_dist[0])
             distance_count += segment_len
 
+            valve_toggleOFF = '\n'
+            valve_toggleON = '\n'
+
             if segment_len < distance_count <= pressure_gradient_region + odd_extra:
+                valve_toggleOFF = valveOFF
+                valve_toggleON = valveON
                 pressure -= pressure_incr
 
             elif pressure < pressure_range[1]:
                 pressure += pressure_incr
 
-            print('Pressure = ', round(pressure, 2))
+            pressure = round(pressure, 2)
+            f.write(valve_toggleOFF)
+            f.write(str('\n\r' + com + '.write(' + str(setpress(pressure)) + ')'))
+            f.write(valve_toggleON)
+
+            print('Pressure = ', pressure)
             if (i+1) == num_segments or (i+1) == 1:
-                print('G1 ' + input_variables[0] + str(sign * first_n_last_segment_len))
+                f.write('\nG1 ' + input_variables[0] + str(sign * first_n_last_segment_len))
             else:
-                print('G1 ' + input_variables[0] + str(sign * segment_len))
+                f.write('\nG1 ' + input_variables[0] + str(sign * segment_len))
 
 
 
@@ -74,16 +169,26 @@ def Gradient_line_segmentation(input_line, segments, pressure_range):
                 a_segment = first_n_last_segment_len / np.sqrt(1 + line_slope ** 2)
                 b_segment = line_slope * a_segment
 
+            valve_toggleOFF = '\n'
+            valve_toggleON = '\n'
+
             if segment_len < distance_count <= pressure_gradient_region + odd_extra:
+                valve_toggleOFF = valveOFF
+                valve_toggleON = valveON
                 pressure -= pressure_incr
 
             elif pressure < pressure_range[1]:
                 pressure += pressure_incr
 
-            print('Pressure = ', round(pressure, 2))
+            pressure = round(pressure, 2)
+            f.write(valve_toggleOFF)
+            f.write(str('\n\r' + com + '.write(' + str(setpress(pressure)) + ')'))
+            f.write(valve_toggleON)
 
-            print('G1 ' + input_variables[0] + str(a_sign*a_segment) + ' ' + input_variables[1] + str(b_sign*b_segment))
-def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range):
+            print('Pressure = ', pressure)
+
+            f.write('\nG1 ' + input_variables[0] + str(a_sign*a_segment) + ' ' + input_variables[1] + str(b_sign*b_segment))
+def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range, valveON, valveOFF,):
     length = ((num_filaments-1)*fil_spacing)
     distance_list = []
     var_list = []
@@ -143,7 +248,7 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
 
 
                 input_line = [['X'], [x_dist]]
-                Gradient_line_segmentation(input_line, segments, pressure_range_filament)
+                Gradient_line_segmentation(input_line, segments, pressure_range_filament, valveON, valveOFF,)
 
                 # distance_list.append([x_dist])
                 # var_list.append(['X'])
@@ -151,7 +256,7 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
                 if (fil + 1) != num_filaments:
                     distance_list.append([y_dist])
                     var_list.append(['Y'])
-                    print('G1 Y' + str(y_dist))
+                    f.write('\nG1 Y' + str(y_dist))
 
             else:
                 x_sign_even_row = x_sign_odd_row * -1 # reverses the x-direction
@@ -165,29 +270,61 @@ def gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, l
 
 
                 input_line = [['Y'], [y_dist]]
-                Gradient_line_segmentation(input_line, segments, pressure_range_filament)
+                Gradient_line_segmentation(input_line, segments, pressure_range_filament, valveON, valveOFF,)
 
                 # distance_list.append([y_dist])
                 # var_list.append(['Y'])
                 if (fil + 1) != num_filaments:
                     distance_list.append([x_dist])
                     var_list.append(['X'])
-                    print('G1 X' + str(x_dist))
+                    f.write('\nG1 X' + str(x_dist))
 
         distance_list.append([layer_height])
         var_list.append(['Z'])
 
-        print('G1 Z' +str(layer_height))
+        f.write('\nG1 Z' +str(layer_height))
 
     # for i in range(len(distance_list)):
     #     print('G1 ' + str(var_list[i][0]) + str(distance_list[i][0]))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    ### File names
+    export_file = '230901_GradientInfillV2_10Layers_2mmspacing_21numfilaments_pressure22_30.txt'
+    save_path = ''#'C:\\Users\\MuellerLab_HPC\\PycharmProjects\\Gcode_generator\\SPropst_Decoupling'
+
+    ### Geometric Settings
     fil_spacing = 2
     num_filaments = 21
     num_layers = 10
     fil_width = 1
     layer_height = 1
     pressure_range = [22, 30]
-    gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range)
+    #gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range)
+
+    ### Pressure box and valve settings
+    com = "serialPort1"
+    valve = 6
+
+    setpress_start = str('\n\r' + com + '.write(' + str(setpress(pressure_range[1])) + ')') # material 1
+    toggleON = str('\n\r'+com +'.write('  + str(togglepress()) + ')') # turn on material 2
+    toggleOFF = toggleON
+
+    valveON = '\n{aux_command}WAGO_ValveCommands(' + str(valve) + ', True)'
+    valveOFF = '\n{aux_command}WAGO_ValveCommands(' + str(valve) + ', False)'
+
+    import os.path
+
+    completeName = os.path.join(save_path, export_file)
+    f = open(completeName, "w")
+    f.write(setpress_start)
+    f.write(toggleON)
+    f.write(valveON)
+
+    gradient_square_lattice(fil_spacing, num_filaments, num_layers, fil_width, layer_height, pressure_range, valveON, valveOFF)
+
+    f.write(valveOFF)
+    f.write(toggleOFF)
+
+    f.close()
+
